927399bd370bf35d6e680540b44cc24090823bee
Junyu Wei <weijunyu@users.noreply.github.com>
2025-01-11 21:16:31 +0800

    Sentry

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import * as Sentry from "@sentry/vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: "https://e7f49d8e90ceabc0b43a7816f10cab28@o4508625204412417.ingest.us.sentry.io/4508625206444032",
+  integrations: [Sentry.replayIntegration()],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+app.mount("#app");
